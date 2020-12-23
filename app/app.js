d88aeace8fc606632cc4878e85b74a9671fe9267
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-12-23 14:17:26 +0100

    add Auth0 protection

--- app/app.js
+++ app/app.js
@@ -13,10 +13,37 @@ app.use(timeout(5000));
 app.use(haltOnTimedout);
 // Variables section
 var PORT = process.env.EXPOSED_PORT || 8082;
-// Routers setup
+// #######################################
+// ###### Auth0 Authorization setup ######
+// #######################################
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+// Authorization middleware. 
+// When used, the Access Token must exist and
+// be verified against the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://robipozzi.eu.auth0.com/.well-known/jwks.json`
+  }),
+  // Validate the audience and the issuer.
+  audience: 'windfire-restaurants',
+  issuer: `https://robipozzi.eu.auth0.com/`,
+  algorithms: ['RS256']
+});
+// ###########################
+// ###### Routers setup ######
+// ###########################
 require("./routers/health")(app, logger);
-require("./routers/restaurant")(app, logger);
-// Run server
+require("./routers/restaurant")(app, checkJwt, logger);
+// ########################
+// ###### Run server ######
+// ########################
 app.listen(PORT, function() {
 	logger.info("Current working directory = " + process.cwd());
 	logger.info("Application is listening on port " + PORT);
--- app/routers/restaurant.js
+++ app/routers/restaurant.js
@@ -3,11 +3,11 @@
 // ############################
 var restaurantService = require('../services/restaurantService');
 
-module.exports = function(app, logger) {
+module.exports = function(app, checkJwt, logger) {
     // #############################
     // ###### HTTP GET method ######
     // #############################
-    app.get('/restaurants', function (httpRequest, httpResponse) {
+    app.get('/restaurants', checkJwt, function (httpRequest, httpResponse) {
         logger.info("GET /restaurants endpoint called");
         logger.info("Calling restaurantService.getRestaurants() ...");
         restaurantService.getRestaurants(function(response) {
@@ -18,7 +18,7 @@ module.exports = function(app, logger) {
     // ##############################
     // ###### HTTP POST method ######
     // ##############################
-    app.post('/restaurants', function (httpRequest, httpResponse) {
+    app.post('/restaurants', checkJwt, function (httpRequest, httpResponse) {
         logger.info("POST /restaurants endpoint called");
         logger.info("Calling restaurantService.addRestaurant() ...");
         restaurantService.addRestaurant(httpRequest.body, function(response) {
@@ -29,7 +29,7 @@ module.exports = function(app, logger) {
     // ################################
     // ###### HTTP DELETE method ######
     // ################################
-    app.delete('/restaurants/:id', function (httpRequest, httpResponse) {
+    app.delete('/restaurants/:id', checkJwt, function (httpRequest, httpResponse) {
         logger.info("DELETE /restaurants endpoint called");
         logger.info("Calling restaurantService.deleteRestaurant() ...");
         restaurantService.deleteRestaurant(httpRequest.params.id, function(response) {
